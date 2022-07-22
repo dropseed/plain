cc83c7a8b74c0ed630e709a1b2c418f44475b196
Dave Gaeddert <dave.gaeddert@gmail.com>
2022-07-22 14:51:16 -0500

    Settings as functions

--- bolt-stafftoolbar/forgestafftoolbar/core.py
+++ bolt-stafftoolbar/forgestafftoolbar/core.py
@@ -7,12 +7,12 @@ from . import settings
 class StaffToolbar:
     def __init__(self, *, request):
         # Callable or list of StaffToolbarLink
-        self.links = settings.STAFFTOOLBAR_LINKS
+        self.links = settings.STAFFTOOLBAR_LINKS()
 
         if callable(self.links):
             self.links = self.links(request)
 
-        self.container_class = settings.STAFFTOOLBAR_CONTAINER_CLASS
+        self.container_class = settings.STAFFTOOLBAR_CONTAINER_CLASS()
 
         self.release = Release()
         self.release.load()
--- bolt-stafftoolbar/forgestafftoolbar/settings.py
+++ bolt-stafftoolbar/forgestafftoolbar/settings.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from .links import StaffToolbarLink
 
 
-def get_STAFFTOOLBAR_LINKS():
+def STAFFTOOLBAR_LINKS():
     return getattr(
         settings,
         "STAFFTOOLBAR_LINKS",
@@ -13,19 +13,5 @@ def get_STAFFTOOLBAR_LINKS():
     )
 
 
-def get_STAFFTOOLBAR_CONTAINER_CLASS():
+def STAFFTOOLBAR_CONTAINER_CLASS():
     return getattr(settings, "STAFFTOOLBAR_CONTAINER_CLASS", "container px-4 mx-auto")
-
-
-def __getattr__(name):
-    """
-    Allows settings to be accessed as settings.SETTING_NAME, just like django.conf.
-    This makes the logic happen at "runtime" rather than when the settings module is imported.
-    """
-    func_name = "get_" + name
-    funcs = globals()
-
-    if func_name in funcs:
-        return funcs[func_name]()
-
-    raise AttributeError(f"{__name__} has no setting named {name}")
