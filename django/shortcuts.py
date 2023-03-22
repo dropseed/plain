7266fd122513851a2343dd369c217ffaa7047b70
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-03-21 22:15:29 -0500

    Delete the render shortcut

--- django/shortcuts.py
+++ django/shortcuts.py
@@ -5,24 +5,12 @@ for convenience's sake.
 """
 from django.http import (
     Http404,
-    HttpResponse,
     HttpResponsePermanentRedirect,
     HttpResponseRedirect,
 )
-from django.template import loader
 from django.urls import NoReverseMatch, reverse
 from django.utils.functional import Promise
 
-
-def render(
-    request, template_name, context=None, content_type=None, status=None, using=None
-):
-    """
-    Return an HttpResponse whose content is filled with the result of calling
-    django.template.loader.render_to_string() with the passed arguments.
-    """
-    content = loader.render_to_string(template_name, context, request, using=using)
-    return HttpResponse(content, content_type, status)
 
 
 def redirect(to, *args, permanent=False, **kwargs):
