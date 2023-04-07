08286532f7583f7daaeecde33321e924e94423ab
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-04-07 14:04:59 -0500

    Remove more cache/middleware stuff

--- django/django/conf/global_settings.py
+++ django/django/conf/global_settings.py
@@ -492,9 +492,6 @@ CACHES = {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
     }
 }
-CACHE_MIDDLEWARE_KEY_PREFIX = ""
-CACHE_MIDDLEWARE_SECONDS = 600
-CACHE_MIDDLEWARE_ALIAS = "default"
 
 ##################
 # AUTHENTICATION #
