ccf395e7c9458d8a4a2b434dc76d965355a607a1
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-04-24 11:07:58 -0500

    Remove site usage in auth LogoutView

--- django/contrib/auth/views.py
+++ django/contrib/auth/views.py
@@ -151,11 +151,8 @@ class LogoutView(RedirectURLMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        current_site = get_current_site(self.request)
         context.update(
             {
-                "site": current_site,
-                "site_name": current_site.name,
                 "title": _("Logged out"),
                 "subtitle": None,
                 **(self.extra_context or {}),
