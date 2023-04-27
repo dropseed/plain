9c3a8f25752ea2d705791624acf2c7fd27eaeb00
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-04-27 14:46:21 -0500

    save

---
+++ bolt-importmap/boltimportmap/jinja.py
@@ -0,0 +1,17 @@
+from django.bolt.jinja.extensions import InclusionTagExtension
+from django.conf import settings
+from .core import Importmap
+import json
+
+class ImportmapScriptsExtension(InclusionTagExtension):
+    tags = {"importmap_scripts"}
+    template_name = "importmap/scripts.html"
+
+    def get_context(self, context, *args, **kwargs):
+        importmap = Importmap()
+        importmap.load()
+
+        if settings.DEBUG:
+            return {"importmap": json.dumps(importmap.map_dev, indent=2, sort_keys=True)}
+        else:
+            return {"importmap": json.dumps(importmap.map, sort_keys=True)}
