4473e3deea23bca5d6a4aa4b866c50275b1b24c1
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-05-10 15:41:41 -0500

    bolt-stripe

--- bolt-stripe/boltstripe/apps.py
+++ bolt-stripe/boltstripe/apps.py
@@ -4,9 +4,9 @@ from django.apps import AppConfig
 from . import settings
 
 
-class ForgestripeConfig(AppConfig):
+class BoltstripeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "forgestripe"
+    name = "boltstripe"
 
     def ready(self):
         if settings.STRIPE_SECRET_KEY():
