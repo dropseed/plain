ec250c57e2aad0246dd33322efafa682af38e017
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-07-07 12:24:39 -0500

    Move autodiscover_modules into django admin itself

--- django/contrib/admin/__init__.py
+++ django/contrib/admin/__init__.py
@@ -20,7 +20,9 @@ from django.contrib.admin.options import (
     TabularInline,
 )
 from django.contrib.admin.sites import AdminSite, site
-from django.utils.module_loading import autodiscover_modules
+from importlib import import_module
+from django.utils.module_loading import module_has_submodule
+import copy
 
 __all__ = [
     "action",
@@ -47,6 +49,42 @@ __all__ = [
     "autodiscover",
 ]
 
+
+# Extracted from utils/module_loading.py
+def autodiscover_modules(*args, **kwargs):
+    """
+    Auto-discover INSTALLED_APPS modules and fail silently when
+    not present. This forces an import on them to register any admin bits they
+    may want.
+
+    You may provide a register_to keyword parameter as a way to access a
+    registry. This register_to object must have a _registry instance variable
+    to access it.
+    """
+    from django.apps import apps
+
+    register_to = kwargs.get("register_to")
+    for app_config in apps.get_app_configs():
+        for module_to_search in args:
+            # Attempt to import the app's module.
+            try:
+                if register_to:
+                    before_import_registry = copy.copy(register_to._registry)
+
+                import_module("%s.%s" % (app_config.name, module_to_search))
+            except Exception:
+                # Reset the registry to the state before the last import
+                # as this import will have to reoccur on the next request and
+                # this could raise NotRegistered and AlreadyRegistered
+                # exceptions (see #8245).
+                if register_to:
+                    register_to._registry = before_import_registry
+
+                # Decide whether to bubble up this error. If the app just
+                # doesn't have the module in question, we can ignore the error
+                # attempting to import it, otherwise we want it to bubble up.
+                if module_has_submodule(app_config.module, module_to_search):
+                    raise
 
 def autodiscover():
     autodiscover_modules("admin", register_to=site)
--- django/utils/module_loading.py
+++ django/utils/module_loading.py
@@ -1,4 +1,3 @@
-import copy
 import os
 import sys
 from importlib import import_module
@@ -33,42 +32,6 @@ def import_string(dotted_path):
             'Module "%s" does not define a "%s" attribute/class'
             % (module_path, class_name)
         ) from err
-
-
-def autodiscover_modules(*args, **kwargs):
-    """
-    Auto-discover INSTALLED_APPS modules and fail silently when
-    not present. This forces an import on them to register any admin bits they
-    may want.
-
-    You may provide a register_to keyword parameter as a way to access a
-    registry. This register_to object must have a _registry instance variable
-    to access it.
-    """
-    from django.apps import apps
-
-    register_to = kwargs.get("register_to")
-    for app_config in apps.get_app_configs():
-        for module_to_search in args:
-            # Attempt to import the app's module.
-            try:
-                if register_to:
-                    before_import_registry = copy.copy(register_to._registry)
-
-                import_module("%s.%s" % (app_config.name, module_to_search))
-            except Exception:
-                # Reset the registry to the state before the last import
-                # as this import will have to reoccur on the next request and
-                # this could raise NotRegistered and AlreadyRegistered
-                # exceptions (see #8245).
-                if register_to:
-                    register_to._registry = before_import_registry
-
-                # Decide whether to bubble up this error. If the app just
-                # doesn't have the module in question, we can ignore the error
-                # attempting to import it, otherwise we want it to bubble up.
-                if module_has_submodule(app_config.module, module_to_search):
-                    raise
 
 
 def module_has_submodule(package, module_name):
