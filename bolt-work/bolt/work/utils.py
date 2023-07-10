89880866d89d4e266ddf9e7ffbdee2257564fdd8
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-07-10 15:38:41 -0500

    Fix bolt work package detection

--- bolt-work/bolt/work/utils.py
+++ bolt-work/bolt/work/utils.py
@@ -6,7 +6,7 @@ import click
 
 def boltpackage_installed(name: str) -> bool:
     try:
-        importlib.import_module(f"bolt{name}")
+        importlib.import_module(f"bolt.{name}")
         return True
     except ImportError:
         return False
