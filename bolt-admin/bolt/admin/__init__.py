a9b9e2c67abfe6119b35301f3f1f83191d184d98
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-08-11 17:27:22 -0500

    Start a trend panel, scroll table

--- bolt-admin/bolt/admin/__init__.py
+++ bolt-admin/bolt/admin/__init__.py
@@ -1,6 +1,11 @@
 from .views.base import AdminPageView
 from .views.models import AdminModelViewset
-from .views.panels import AdminChartPanelView, AdminPanelView, AdminTextPanelView
+from .views.panels import (
+    AdminChartPanelView,
+    AdminPanelView,
+    AdminTextPanelView,
+    AdminTrendPanelView,
+)
 from .views.registry import register_model, register_panel, register_view
 
 __all__ = [
@@ -9,6 +14,7 @@ __all__ = [
     "AdminPanelView",
     "AdminTextPanelView",
     "AdminChartPanelView",
+    "AdminTrendPanelView",
     "register_model",
     "register_panel",
     "register_view",
--- bolt-admin/bolt/admin/views/panels.py
+++ bolt-admin/bolt/admin/views/panels.py
@@ -1,4 +1,9 @@
+import datetime
+from datetime import timedelta
 from enum import Enum
+
+from django.utils import timezone
+from django.utils.functional import cached_property
 
 from .base import BaseAdminView
 
@@ -47,3 +52,91 @@ class AdminChartPanelView(AdminPanelView):
 
     def get_chart_data(self) -> dict:
         raise NotImplementedError
+
+
+class DateRange:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+        if isinstance(self.start, datetime.datetime):
+            self.start = self.start.date()
+
+        if isinstance(self.end, datetime.datetime):
+            self.end = self.end.date()
+
+    def days(self):
+        return (self.end - self.start).days
+
+    def __iter__(self):
+        return iter(self.start + timedelta(days=i) for i in range(0, self.days()))
+
+    def __repr__(self):
+        return f"DateRange({self.start}, {self.end})"
+
+    def __str__(self):
+        return f"{self.start} - {self.end}"
+
+    def __eq__(self, other):
+        return self.start == other.start and self.end == other.end
+
+    def __hash__(self):
+        return hash((self.start, self.end))
+
+    def __contains__(self, item):
+        return self.start <= item <= self.end
+
+
+class AdminTrendPanelView(AdminChartPanelView):
+    class Ranges(Enum):
+        LAST_365_DAYS = "last_365_days"
+        LAST_30_DAYS = "last_30_days"
+        LAST_7_DAYS = "last_7_days"
+
+    range: Ranges = Ranges.LAST_30_DAYS
+
+    @cached_property
+    def date_range(self) -> DateRange:
+        return self.get_date_range()
+
+    def get_date_range(self) -> DateRange:
+        now = timezone.now()
+
+        if self.range == self.Ranges.LAST_365_DAYS:
+            return DateRange(now - timedelta(days=365), now)
+
+        if self.range == self.Ranges.LAST_30_DAYS:
+            return DateRange(now - timedelta(days=30), now)
+
+        if self.range == self.Ranges.LAST_7_DAYS:
+            return DateRange(now - timedelta(days=7), now)
+
+        raise ValueError(f"Invalid range: {self.range}")
+
+    def get_values(self) -> dict[datetime.date, int]:
+        raise NotImplementedError
+
+    def get_chart_data(self) -> dict:
+        date_labels = [date.strftime("%Y-%m-%d") for date in self.date_range]
+        date_values = self.get_values()
+
+        for date in self.date_range:
+            if date not in date_values:
+                date_values[date] = 0
+
+        # Sort the date values
+        data = [date_values[date] for date in sorted(date_values.keys())]
+
+        return {
+            "type": "line",
+            "data": {
+                "labels": date_labels,
+                "datasets": [
+                    {
+                        "data": data,
+                    }
+                ],
+            },
+            # Hide the label
+            # "options": {"legend": {"display": False}},
+        }
