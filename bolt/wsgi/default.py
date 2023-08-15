10d44333d3e942193d82c45161a6630a4395a1e9
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-08-15 15:07:11 -0500

    Move django.core.wsgi

--- bolt/wsgi/default.py
+++ bolt/wsgi/default.py
@@ -1,4 +1,16 @@
-from django.core.wsgi import get_wsgi_application
+import django
+from django.core.handlers.wsgi import WSGIHandler
+
+
+def get_wsgi_application():
+    """
+    The public interface to Django's WSGI support. Return a WSGI callable.
+
+    Avoids making django.core.handlers.WSGIHandler a public API, in case the
+    internal WSGI implementation changes or moves in the future.
+    """
+    django.setup(set_prefix=False)
+    return WSGIHandler()
 
 
 application = get_wsgi_application()
