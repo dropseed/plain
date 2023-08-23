bf05b562279f519100fd098e9c981c2d8acafe57
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-08-23 11:42:02 -0500

    Remove RUNNING_DJANGOS_TEST_SUITE cases

--- bolt-db/bolt/db/backends/base/creation.py
+++ bolt-db/bolt/db/backends/base/creation.py
@@ -99,9 +99,6 @@ class BaseDatabaseCreation:
         # Ensure a connection for the side effect of initializing the test database.
         self.connection.ensure_connection()
 
-        if os.environ.get("RUNNING_DJANGOS_TEST_SUITE") == "true":
-            self.mark_expected_failures_and_skips()
-
         return test_database_name
 
     def set_as_test_mirror(self, primary_settings_dict):
@@ -324,32 +321,6 @@ class BaseDatabaseCreation:
             cursor.execute(
                 "DROP DATABASE %s" % self.connection.ops.quote_name(test_database_name)
             )
-
-    def mark_expected_failures_and_skips(self):
-        """
-        Mark tests in Django's test suite which are expected failures on this
-        database and test which should be skipped on this database.
-        """
-        # Only load unittest if we're actually testing.
-        from unittest import expectedFailure, skip
-
-        for test_name in self.connection.features.django_test_expected_failures:
-            test_case_name, _, test_method_name = test_name.rpartition(".")
-            test_app = test_name.split(".")[0]
-            # Importing a test app that isn't installed raises RuntimeError.
-            if test_app in settings.INSTALLED_APPS:
-                test_case = import_string(test_case_name)
-                test_method = getattr(test_case, test_method_name)
-                setattr(test_case, test_method_name, expectedFailure(test_method))
-        for reason, tests in self.connection.features.django_test_skips.items():
-            for test_name in tests:
-                test_case_name, _, test_method_name = test_name.rpartition(".")
-                test_app = test_name.split(".")[0]
-                # Importing a test app that isn't installed raises RuntimeError.
-                if test_app in settings.INSTALLED_APPS:
-                    test_case = import_string(test_case_name)
-                    test_method = getattr(test_case, test_method_name)
-                    setattr(test_case, test_method_name, skip(reason)(test_method))
 
     def sql_table_creation_suffix(self):
         """
