063632340bf24f135498468b78a6a53606271b09
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-09-02 13:20:40 -0500

    Add cryptography and remove rich

--- bolt/cli/cli.py
+++ bolt/cli/cli.py
@@ -5,11 +5,6 @@ import subprocess
 import sys
 
 import click
-from rich import box
-from rich.console import Console
-from rich.pretty import Pretty
-from rich.table import Table
-from rich.text import Text
 
 import bolt.runtime
 from bolt.runtime import settings
@@ -183,50 +178,50 @@ def run(script):
         sys.exit(result.returncode)
 
 
-@bolt_cli.command()
-@click.option("--filter", "-f", "name_filter", help="Filter settings by name")
-@click.option("--overridden", is_flag=True, help="Only show overridden settings")
-def settings(name_filter, overridden):
-    """Print Bolt settings"""
-    table = Table(box=box.MINIMAL)
-    table.add_column("Setting")
-    table.add_column("Default value")
-    table.add_column("App value")
-    table.add_column("Type")
-    table.add_column("Module")
+# @bolt_cli.command()
+# @click.option("--filter", "-f", "name_filter", help="Filter settings by name")
+# @click.option("--overridden", is_flag=True, help="Only show overridden settings")
+# def settings(name_filter, overridden):
+#     """Print Bolt settings"""
+#     table = Table(box=box.MINIMAL)
+#     table.add_column("Setting")
+#     table.add_column("Default value")
+#     table.add_column("App value")
+#     table.add_column("Type")
+#     table.add_column("Module")
 
-    for setting in dir(settings):
-        if setting.isupper():
-            if name_filter and name_filter.upper() not in setting:
-                continue
+#     for setting in dir(settings):
+#         if setting.isupper():
+#             if name_filter and name_filter.upper() not in setting:
+#                 continue
 
-            is_overridden = settings.is_overridden(setting)
+#             is_overridden = settings.is_overridden(setting)
 
-            if overridden and not is_overridden:
-                continue
+#             if overridden and not is_overridden:
+#                 continue
 
-            default_setting = settings._default_settings.get(setting)
-            if default_setting:
-                default_value = default_setting.value
-                annotation = default_setting.annotation
-                module = default_setting.module
-            else:
-                default_value = ""
-                annotation = ""
-                module = ""
+#             default_setting = settings._default_settings.get(setting)
+#             if default_setting:
+#                 default_value = default_setting.value
+#                 annotation = default_setting.annotation
+#                 module = default_setting.module
+#             else:
+#                 default_value = ""
+#                 annotation = ""
+#                 module = ""
 
-            table.add_row(
-                setting,
-                Pretty(default_value) if default_value else "",
-                Pretty(getattr(settings, setting))
-                if is_overridden
-                else Text("<Default>", style="italic dim"),
-                Pretty(annotation) if annotation else "",
-                str(module.__name__) if module else "",
-            )
+#             table.add_row(
+#                 setting,
+#                 Pretty(default_value) if default_value else "",
+#                 Pretty(getattr(settings, setting))
+#                 if is_overridden
+#                 else Text("<Default>", style="italic dim"),
+#                 Pretty(annotation) if annotation else "",
+#                 str(module.__name__) if module else "",
+#             )
 
-    console = Console()
-    console.print(table)
+#     console = Console()
+#     console.print(table)
 
 
 @bolt_cli.command()
