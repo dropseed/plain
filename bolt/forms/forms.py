e71c609fddef27acd75b22aaee822ebbb86c1669
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-09-13 14:48:42 -0500

    Remove special datetime handling in form value

--- bolt/forms/forms.py
+++ bolt/forms/forms.py
@@ -295,13 +295,6 @@ class BaseForm:
         value = self.initial.get(field_name, field.initial)
         if callable(value):
             value = value()
-        # If this is an auto-generated default date, nix the microseconds
-        # for standardized handling. See #22502.
-        if (
-            isinstance(value, datetime.datetime | datetime.time)
-            and not field.widget.supports_microseconds
-        ):
-            value = value.replace(microsecond=0)
         return value
 
 
