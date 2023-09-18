c7c44734b4e300b79b60914a88de5db60b2a84ef
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-09-18 15:36:02 -0500

    Move unknown database table check into bolt.db

--- bolt-db/bolt/db/checks.py
+++ bolt-db/bolt/db/checks.py
@@ -239,3 +239,46 @@ def _check_lazy_references(packages, ignore=None):
 @register(Tags.models)
 def check_lazy_references(package_configs=None, **kwargs):
     return _check_lazy_references(packages)
+
+
+@register(Tags.database)
+def check_database_tables(package_configs, **kwargs):
+    from bolt.db import connection
+
+    databases = kwargs.get("databases", None)
+    if not databases:
+        return []
+
+    errors = []
+
+    cache_tables = [
+        x["LOCATION"]
+        for x in settings.CACHES.values()
+        if x["BACKEND"] == "bolt.cache.backends.db.DatabaseCache"
+    ]
+
+    for database in databases:
+        db_tables = connection.introspection.table_names()
+        model_tables = connection.introspection.bolt_table_names()
+
+        unknown_tables = set(db_tables) - set(model_tables) - set(cache_tables)
+        unknown_tables.discard("django_migrations")  # Know this could be there
+        if unknown_tables:
+            table_names = ", ".join(unknown_tables)
+            specific_hint = (
+                f'echo "DROP TABLE IF EXISTS {unknown_tables.pop()}" | '
+                + "bolt db shell"
+            )
+            errors.append(
+                Error(
+                    f"Unknown tables in {database} database: {table_names}",
+                    hint=(
+                        "Tables may be from packages/models that have been uninstalled. "
+                        + "Make sure you have a backup and delete the tables manually "
+                        + f"(ex. `{specific_hint}`)."
+                    ),
+                    id="bolt.db.E001",
+                )
+            )
+
+    return errors
