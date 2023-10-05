768e749849138982cd32f8cca68f928c7e2b3834
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-10-05 12:58:50 -0500

    Load app.shell on cli startup

--- bolt/cli/startup.py
+++ bolt/cli/startup.py
@@ -2,6 +2,31 @@ import bolt.runtime
 
 bolt.runtime.setup()
 
-print("\033[1m", end="")
-print("🔩 Welcome to the Bolt shell!")
-print("\033[0m", end="")
+
+def _print_bold(s):
+    print("\033[1m", end="")
+    print(s)
+    print("\033[0m", end="")
+
+def _print_italic(s):
+    print("\x1B[3m", end="")
+    print(s)
+    print("\x1B[0m", end="")
+
+
+_print_bold("\n⬣ Welcome to the Bolt shell! ⬣")
+
+_app_shell = bolt.runtime.APP_PATH / "shell.py"
+
+if _app_shell.exists():
+    _print_bold("\nImporting custom app/shell.py")
+    contents = _app_shell.read_text()
+
+    for line in contents.splitlines():
+        _print_italic(f">>> {line}")
+
+    print()
+
+    # Import * so we get everything that file imported
+    # (which is mostly the point of having it)
+    from app.shell import *  # noqa
