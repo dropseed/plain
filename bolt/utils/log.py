a1f533f951053e9574f2b3e46bee1d1b90d914be
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-10-11 11:39:21 -0500

    Remove unused logging filters and formatters

--- bolt/utils/log.py
+++ bolt/utils/log.py
@@ -2,9 +2,6 @@ import logging
 import logging.config
 from os import environ
 
-from bolt.legacy.management.color import color_style
-from bolt.runtime import settings
-
 request_logger = logging.getLogger("bolt.request")
 
 
@@ -13,14 +10,6 @@ def configure_logging(logging_settings):
     default_logging = {
         "version": 1,
         "disable_existing_loggers": False,
-        "filters": {
-            "require_debug_false": {
-                "()": "bolt.utils.log.RequireDebugFalse",
-            },
-            "require_debug_true": {
-                "()": "bolt.utils.log.RequireDebugTrue",
-            },
-        },
         "formatters": {
             "simple": {
                 "format": "[%(levelname)s] %(message)s",
@@ -50,71 +39,6 @@ def configure_logging(logging_settings):
     # Then customize it from settings
     if logging_settings:
         logging.config.dictConfig(logging_settings)
-
-
-class CallbackFilter(logging.Filter):
-    """
-    A logging filter that checks the return value of a given callable (which
-    takes the record-to-be-logged as its only parameter) to decide whether to
-    log a record.
-    """
-
-    def __init__(self, callback):
-        self.callback = callback
-
-    def filter(self, record):
-        if self.callback(record):
-            return 1
-        return 0
-
-
-class RequireDebugFalse(logging.Filter):
-    def filter(self, record):
-        return not settings.DEBUG
-
-
-class RequireDebugTrue(logging.Filter):
-    def filter(self, record):
-        return settings.DEBUG
-
-
-class ServerFormatter(logging.Formatter):
-    default_time_format = "%d/%b/%Y %H:%M:%S"
-
-    def __init__(self, *args, **kwargs):
-        self.style = color_style()
-        super().__init__(*args, **kwargs)
-
-    def format(self, record):
-        msg = record.msg
-        status_code = getattr(record, "status_code", None)
-
-        if status_code:
-            if 200 <= status_code < 300:
-                # Put 2XX first, since it should be the common case
-                msg = self.style.HTTP_SUCCESS(msg)
-            elif 100 <= status_code < 200:
-                msg = self.style.HTTP_INFO(msg)
-            elif status_code == 304:
-                msg = self.style.HTTP_NOT_MODIFIED(msg)
-            elif 300 <= status_code < 400:
-                msg = self.style.HTTP_REDIRECT(msg)
-            elif status_code == 404:
-                msg = self.style.HTTP_NOT_FOUND(msg)
-            elif 400 <= status_code < 500:
-                msg = self.style.HTTP_BAD_REQUEST(msg)
-            else:
-                # Any 5XX, or any other status code
-                msg = self.style.HTTP_SERVER_ERROR(msg)
-
-        if self.uses_server_time() and not hasattr(record, "server_time"):
-            record.server_time = self.formatTime(record, self.datefmt)
-
-        record.msg = msg
-        return super().format(record)
-
-    def uses_server_time(self):
-        return self._fmt.find("{server_time}") >= 0
 
 
 def log_response(
