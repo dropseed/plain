fef4e64555c6a959d3c7ca344f794707f2f01901
Dave Gaeddert <dave.gaeddert@gmail.com>
2023-10-16 17:28:47 -0500

    Turn on more ruff selectors

--- bolt-querystats/tests/test_sql.py
+++ bolt-querystats/tests/test_sql.py
@@ -4,5 +4,5 @@ from boltquerystats.sql import pretty_print_sql
 def test_pretty_print_sql():
     sql = "SELECT * FROM foo WHERE bar = 'baz' ORDER BY baz LIMIT 10"
     assert pretty_print_sql(sql) == (
-        "SELECT *\n" "FROM foo\n" "WHERE bar = 'baz'\n" "ORDER BY baz\n" "LIMIT 10"
+        "SELECT *\nFROM foo\nWHERE bar = 'baz'\nORDER BY baz\nLIMIT 10"
     )
