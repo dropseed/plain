6cb60d1bd2ed832c4c4ef24c456952db7dbca078
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-01-12 11:36:34 -0600

    Format with ruff

--- bolt-db/bolt/db/default_settings.py
+++ bolt-db/bolt/db/default_settings.py
@@ -8,7 +8,8 @@ DATABASES = {
         environ["DATABASE_URL"],
         # Enable persistent connections by default
         conn_max_age=int(environ.get("DATABASE_CONN_MAX_AGE", 600)),
-        conn_health_checks=environ.get("DATABASE_CONN_HEALTH_CHECKS", "true").lower() in [
+        conn_health_checks=environ.get("DATABASE_CONN_HEALTH_CHECKS", "true").lower()
+        in [
             "true",
             "1",
         ],
