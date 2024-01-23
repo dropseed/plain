6e760463f4c2257c3dde6fccb0dca6dc9db3aa75
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-01-23 10:55:02 -0600

    Check for empty value in localtime filter

--- bolt/templates/jinja/filters.py
+++ bolt/templates/jinja/filters.py
@@ -5,12 +5,22 @@ from bolt.utils.html import json_script
 from bolt.utils.timesince import timesince, timeuntil
 from bolt.utils.timezone import localtime
 
+
+def localtime_filter(value, timezone=None):
+    """Converts a datetime to local time in a template."""
+    if not value:
+        # Without this, we get the current localtime
+        # which doesn't make sense as a filter
+        return ""
+    return localtime(value, timezone)
+
+
 default_filters = {
     # The standard Python ones
     "strftime": datetime.datetime.strftime,
     "strptime": datetime.datetime.strptime,
     # To convert to user time zone
-    "localtime": localtime,
+    "localtime": localtime_filter,
     "timeuntil": timeuntil,
     "timesince": timesince,
     "json_script": json_script,
