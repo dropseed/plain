1b1f2461722e6756d9459fd2ed94996df8f8e5c3
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-02-12 17:42:46 -0600

    Remove make_random_password

--- bolt-auth/bolt/auth/models.py
+++ bolt-auth/bolt/auth/models.py
@@ -60,23 +60,6 @@ class UserManager(models.Manager):
             email = email_name + "@" + domain_part.lower()
         return email
 
-    def make_random_password(
-        self,
-        length=10,
-        allowed_chars="abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789",
-    ):
-        """
-        Generate a random password with the given length and given
-        allowed_chars. The default value of allowed_chars does not have "I" or
-        "O" or letters and digits that look similar -- just to avoid confusion.
-        """
-        warnings.warn(
-            "BaseUserManager.make_random_password() is deprecated.",
-            category=RemovedInDjango51Warning,
-            stacklevel=2,
-        )
-        return get_random_string(length, allowed_chars)
-
     def get_by_natural_key(self, username):
         return self.get(**{self.model.USERNAME_FIELD: username})
 
