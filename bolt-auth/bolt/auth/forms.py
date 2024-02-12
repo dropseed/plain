ab99e9655358f9bb4a3dbc453d4b8a034df11fe2
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-02-12 17:57:45 -0600

    Remove read only password widgets

--- bolt-auth/bolt/auth/forms.py
+++ bolt-auth/bolt/auth/forms.py
@@ -23,45 +23,6 @@ def _unicode_ci_compare(s1, s2):
     )
 
 
-# class ReadOnlyPasswordHashWidget(forms.Widget):
-#     template_name = "auth/widgets/read_only_password_hash.html"
-#     read_only = True
-
-#     def get_context(self, name, value, attrs):
-#         context = super().get_context(name, value, attrs)
-#         summary = []
-#         if not value or value.startswith(UNUSABLE_PASSWORD_PREFIX):
-#             summary.append({"label": gettext("No password set.")})
-#         else:
-#             try:
-#                 hasher = identify_hasher(value)
-#             except ValueError:
-#                 summary.append(
-#                     {
-#                         "label": gettext(
-#                             "Invalid password format or unknown hashing algorithm."
-#                         )
-#                     }
-#                 )
-#             else:
-#                 for key, value_ in hasher.safe_summary(value).items():
-#                     summary.append({"label": gettext(key), "value": value_})
-#         context["summary"] = summary
-#         return context
-
-#     def id_for_label(self, id_):
-#         return None
-
-
-class ReadOnlyPasswordHashField(forms.Field):
-    # widget = ReadOnlyPasswordHashWidget
-
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("required", False)
-        kwargs.setdefault("disabled", True)
-        super().__init__(*args, **kwargs)
-
-
 class UsernameField(forms.CharField):
     def to_python(self, value):
         return unicodedata.normalize("NFKC", super().to_python(value))
