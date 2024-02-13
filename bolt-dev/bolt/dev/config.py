de38a479ec2c8bf6bde971bb2be558e45d5a067a
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-02-12 22:02:50 -0600

    Make .bolt in dev symlink if it doesn't exist

--- bolt-dev/bolt/dev/config.py
+++ bolt-dev/bolt/dev/config.py
@@ -11,6 +11,8 @@ class Config(PackageConfig):
     def ready(self):
         # Symlink the bolt package into .bolt so we can look at it easily
         bolt_path = Path(importlib.util.find_spec("bolt.runtime").origin).parent.parent
+        if not settings.BOLT_TEMP_PATH.exists():
+            settings.BOLT_TEMP_PATH.mkdir()
         src_path = settings.BOLT_TEMP_PATH / "src"
         if bolt_path.exists() and not src_path.exists():
             src_path.symlink_to(bolt_path)
