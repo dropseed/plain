35eb618d811ac1f26aa0c6a860f3b5a7fe726b87
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-03-18 12:21:18 -0500

    Add oauth admin

---
+++ bolt-oauth/bolt/oauth/admin.py
@@ -0,0 +1,45 @@
+from bolt.admin.cards import ChartCard
+from bolt.admin.views import (
+    AdminModelDetailView,
+    AdminModelListView,
+    AdminModelViewset,
+    register_viewset,
+)
+from bolt.db.models import Count
+
+from .models import OAuthConnection
+
+
+class ProvidersChartCard(ChartCard):
+    title = "Providers"
+
+    def get_chart_data(self) -> dict:
+        results = (
+            OAuthConnection.objects.all()
+            .values("provider_key")
+            .annotate(count=Count("id"))
+        )
+        return {
+            "type": "doughnut",
+            "data": {
+                "labels": [result["provider_key"] for result in results],
+                "datasets": [
+                    {
+                        "label": "Providers",
+                        "data": [result["count"] for result in results],
+                    }
+                ],
+            },
+        }
+
+
+@register_viewset
+class OAuthConnectionViewset(AdminModelViewset):
+    class ListView(AdminModelListView):
+        nav_section = "OAuth"
+        model = OAuthConnection
+        fields = ["id", "user", "provider_key", "provider_user_id"]
+        cards = [ProvidersChartCard]
+
+    class DetailView(AdminModelDetailView):
+        model = OAuthConnection
