a06a2ba0628c4df5a4434fd76700554fef0a65a0
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-03-25 16:19:49 -0500

    Remove unused html_safe function

--- bolt/utils/html.py
+++ bolt/utils/html.py
@@ -399,24 +399,3 @@ def avoid_wrapping(value):
     spaces where there previously were normal spaces.
     """
     return value.replace(" ", "\xa0")
-
-
-def html_safe(klass):
-    """
-    A decorator that defines the __html__ method. This helps non-Bolt
-    templates to detect classes whose __str__ methods return SafeString.
-    """
-    if "__html__" in klass.__dict__:
-        raise ValueError(
-            "can't apply @html_safe to %s because it defines "
-            "__html__()." % klass.__name__
-        )
-    if "__str__" not in klass.__dict__:
-        raise ValueError(
-            "can't apply @html_safe to %s because it doesn't "
-            "define __str__()." % klass.__name__
-        )
-    klass_str = klass.__str__
-    klass.__str__ = lambda self: mark_safe(klass_str(self))
-    klass.__html__ = lambda self: str(self)
-    return klass
