a0107999cf75fabc687fefb172170b14151f86d3
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-04-16 17:24:22 -0500

    Remove the app ready module loading (no worker registry anymore)

--- bolt-worker/bolt/worker/config.py
+++ bolt-worker/bolt/worker/config.py
@@ -1,6 +1,4 @@
-from importlib import import_module
-
-from bolt.packages import PackageConfig, packages
+from bolt.packages import PackageConfig
 
 MODULE_NAME = "jobs"
 
@@ -8,17 +6,3 @@ MODULE_NAME = "jobs"
 class BoltJobsConfig(PackageConfig):
     name = "bolt.worker"
     label = "boltqueue"
-
-    def ready(self):
-        # Trigger register calls to fire by importing the modules
-        for package_config in packages.get_package_configs():
-            try:
-                import_module(f"{package_config.name}.{MODULE_NAME}")
-            except ModuleNotFoundError:
-                pass
-
-        # Also trigger for the root app/admin.py module
-        try:
-            import_module(MODULE_NAME)
-        except ModuleNotFoundError:
-            pass
