f543d70c0b90799568185a4fa43c06f230d948d3
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-05-28 15:14:43 -0500

    Slugify and add id to headings in pages content

--- bolt-pages/bolt/pages/markdown.py
+++ bolt-pages/bolt/pages/markdown.py
@@ -3,18 +3,31 @@ from pygments import highlight
 from pygments.formatters import html
 from pygments.lexers import get_lexer_by_name
 
+from bolt.utils.text import slugify
 
-class HighlightRenderer(mistune.HTMLRenderer):
+
+class PagesRenderer(mistune.HTMLRenderer):
+    def heading(self, text, level, **attrs):
+        """Automatically add an ID to headings if one is not provided."""
+
+        if "id" not in attrs:
+            attrs["id"] = slugify(text)
+
+        return super().heading(text, level, **attrs)
+
     def block_code(self, code, info=None):
+        """Highlight code blocks using Pygments."""
+
         if info:
             lexer = get_lexer_by_name(info, stripall=True)
             formatter = html.HtmlFormatter()
             return highlight(code, lexer, formatter)
+
         return "<pre><code>" + mistune.escape(code) + "</code></pre>"
 
 
 def render_markdown(content):
-    renderer = HighlightRenderer(escape=False)
+    renderer = PagesRenderer(escape=False)
     markdown = mistune.create_markdown(
         renderer=renderer, plugins=["strikethrough", "table"]
     )
