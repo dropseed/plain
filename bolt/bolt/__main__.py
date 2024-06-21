2ab5f317e6fb67259a51bebe3572f2316836d890
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-06-21 16:33:45 -0500

    Code comments

--- bolt/bolt/__main__.py
+++ bolt/bolt/__main__.py
@@ -1,4 +1,5 @@
 from .cli import cli
 
+# Make the CLI available as `python -m bolt`
 if __name__ == "__main__":
     cli()
--- bolt/bolt/wsgi.py
+++ bolt/bolt/wsgi.py
@@ -2,15 +2,10 @@ import bolt.runtime
 from bolt.internal.handlers.wsgi import WSGIHandler
 
 
-def get_wsgi_application():
-    """
-    The public interface to Bolt's WSGI support. Return a WSGI callable.
-
-    Avoids making bolt.internal.handlers.WSGIHandler a public API, in case the
-    internal WSGI implementation changes or moves in the future.
-    """
+def _get_wsgi_application():
     bolt.runtime.setup()
     return WSGIHandler()
 
 
-app = get_wsgi_application()
+# The default `bolt.wsgi:app` WSGI application
+app = _get_wsgi_application()
