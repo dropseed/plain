4d1d3ab2fde7fc7e76e09acf077cdcf5028d165e
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-09-26 14:21:29 -0500

    Remove unused resolve_url

--- plain-passwords/plain/passwords/views.py
+++ plain-passwords/plain/passwords/views.py
@@ -4,8 +4,6 @@ from plain.auth.sessions import login as auth_login
 from plain.http import (
     ResponseRedirect,
 )
-from plain.urls import NoReverseMatch, reverse
-from plain.utils.functional import Promise
 from plain.views import CreateView, FormView
 
 from .forms import (
@@ -15,47 +13,6 @@ from .forms import (
     PasswordLoginForm,
     PasswordSignupForm,
 )
-
-
-def resolve_url(to, *args, **kwargs):
-    """
-    Return a URL appropriate for the arguments passed.
-
-    The arguments could be:
-
-        * A model: the model's `get_absolute_url()` function will be called.
-
-        * A view name, possibly with arguments: `urls.reverse()` will be used
-          to reverse-resolve the name.
-
-        * A URL, which will be returned as-is.
-    """
-    # If it's a model, use get_absolute_url()
-    if hasattr(to, "get_absolute_url"):
-        return to.get_absolute_url()
-
-    if isinstance(to, Promise):
-        # Expand the lazy instance, as it can cause issues when it is passed
-        # further to some Python functions like urlparse.
-        to = str(to)
-
-    # Handle relative URLs
-    if isinstance(to, str) and to.startswith(("./", "../")):
-        return to
-
-    # Next try a reverse URL resolution.
-    try:
-        return reverse(to, args=args, kwargs=kwargs)
-    except NoReverseMatch:
-        # If this is a callable, re-raise.
-        if callable(to):
-            raise
-        # If this doesn't "feel" like a URL, re-raise.
-        if "/" not in to and "." not in to:
-            raise
-
-    # Finally, fall back and assume it's a URL
-    return to
 
 
 def update_session_auth_hash(request, user):
