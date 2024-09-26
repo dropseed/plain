a1068a024d658163af98b09b3d8067cc4dce74d2
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-09-26 11:37:56 -0500

    Use uv in contrib cli

--- plain-dev/plain/dev/contribute/cli.py
+++ plain-dev/plain/dev/contribute/cli.py
@@ -3,7 +3,6 @@ import sys
 from pathlib import Path
 
 import click
-import tomllib
 
 
 @click.command("contribute")
@@ -13,22 +12,16 @@ def cli(package, repo):
     """Contribute to plain by linking a package locally."""
 
     if package == "reset":
-        click.secho("Undoing any changes to pyproject.toml and poetry.lock", bold=True)
-        result = subprocess.run(["git", "checkout", "pyproject.toml", "poetry.lock"])
+        click.secho("Undoing any changes to pyproject.toml and uv.lock", bold=True)
+        result = subprocess.run(["git", "checkout", "pyproject.toml", "uv.lock"])
         if result.returncode:
-            click.secho("Failed to checkout pyproject.toml and poetry.lock", fg="red")
+            click.secho("Failed to checkout pyproject.toml and uv.lock", fg="red")
             sys.exit(result.returncode)
 
-        click.secho("Removing current .venv", bold=True)
-        result = subprocess.run(["rm", "-rf", ".venv"])
+        click.secho("Running uv sync", bold=True)
+        result = subprocess.run(["uv", "sync"])
         if result.returncode:
-            click.secho("Failed to remove .venv", fg="red")
-            sys.exit(result.returncode)
-
-        click.secho("Running poetry install", bold=True)
-        result = subprocess.run(["poetry", "install"])
-        if result.returncode:
-            click.secho("Failed to install", fg="red")
+            click.secho("Failed to sync", fg="red")
             sys.exit(result.returncode)
 
         return
@@ -53,42 +46,14 @@ def cli(package, repo):
     )
     click.secho(f"Using repo at {repo} ({repo_branch} branch)", bold=True)
 
-    pyproject = Path("pyproject.toml")
-    if not pyproject.exists():
-        click.secho("pyproject.toml not found", fg="red")
-        return
-
-    poetry_group = "main"
-
-    with pyproject.open("rb") as f:
-        pyproject_data = tomllib.load(f)
-        poetry_dependencies = (
-            pyproject_data.get("tool", {}).get("poetry", {}).get("dependencies", {})
-        )
-
-        for group_name, group_data in (
-            pyproject_data.get("tool", {}).get("poetry", {}).get("group", {}).items()
-        ):
-            if package in group_data.get("dependencies", {}).keys():
-                poetry_group = group_name
-                break
-
-        if not poetry_group and package not in poetry_dependencies.keys():
-            click.secho(
-                f"{package} not found in pyproject.toml (only poetry is supported)",
-                fg="red",
-            )
-            return
-
     click.secho(f"Linking {package} to {repo}", bold=True)
     if package == "plain" or package.startswith("plain-"):
         result = subprocess.run(
             [
-                "poetry",
+                "uv",
                 "add",
                 "--editable",
-                "--group",
-                poetry_group,
+                "--dev",
                 str(repo / package),  # Link a subdirectory
             ]
         )
@@ -98,11 +63,10 @@ def cli(package, repo):
     elif package.startswith("plainx-"):
         result = subprocess.run(
             [
-                "poetry",
+                "uv",
                 "add",
                 "--editable",
-                "--group",
-                poetry_group,
+                "--dev",
                 str(repo),
             ]
         )
