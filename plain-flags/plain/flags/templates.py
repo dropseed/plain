69b88990b94f03d1f5e9df5b1fde9dd03a456da4
Dave Gaeddert <dave.gaeddert@gmail.com>
2024-12-16 15:56:04 -0600

    Fix flags template global

--- plain-flags/plain/flags/templates.py
+++ plain-flags/plain/flags/templates.py
@@ -2,4 +2,4 @@ from plain.templates import register_template_global
 
 from .bridge import get_flags_module
 
-register_template_global("flags", get_flags_module())
+register_template_global(get_flags_module(), name="flags")
