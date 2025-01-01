515eb4c4632c5e71996a2c032693d95ac5dfbe1e
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-01-01 17:24:18 -0600

    Switch back to using a PLAIN_ENV optionally

--- plain-dev/plain/dev/entrypoints.py
+++ plain-dev/plain/dev/entrypoints.py
@@ -1,9 +1,10 @@
+import os
+
 from dotenv import load_dotenv
 
 
 def load_dotenv_file():
-    loaded = load_dotenv(".env")
-    if not loaded:
-        # In CI-like environments, we may have .env.test committed
-        # but not .env, so we can help out by loading it here.
-        load_dotenv(".env.test")
+    if plain_env := os.environ.get("PLAIN_ENV", ""):
+        load_dotenv(f".env.{plain_env}")
+    else:
+        load_dotenv(".env")
