3aeb4492d0e15694b9e0f0088c9f85f792dcf57f
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-01-01 17:21:52 -0600

    Load .env.test automatically if .env did nothing

--- plain-dev/plain/dev/entrypoints.py
+++ plain-dev/plain/dev/entrypoints.py
@@ -2,4 +2,8 @@ from dotenv import load_dotenv
 
 
 def load_dotenv_file():
-    load_dotenv(".env")
+    loaded = load_dotenv(".env")
+    if not loaded:
+        # In CI-like environments, we may have .env.test committed
+        # but not .env, so we can help out by loading it here.
+        load_dotenv(".env.test")
