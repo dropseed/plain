17b4e06b0f87188399b74ea961b3526cd4125c90
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-01-06 14:20:17 -0600

    Make pageview uuid unique

--- plain-pageviews/plain/pageviews/models.py
+++ plain-pageviews/plain/pageviews/models.py
@@ -4,7 +4,7 @@ from plain import models
 
 
 class Pageview(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     url = models.URLField(max_length=1024, db_index=True)
     timestamp = models.DateTimeField(db_index=True, auto_now_add=True)
