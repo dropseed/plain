4849de7d8c2068f0d43c5af2029ea5c14bb8afa3
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-01-06 14:17:38 -0600

    Increase Pageview max_lengths

--- plain-pageviews/plain/pageviews/models.py
+++ plain-pageviews/plain/pageviews/models.py
@@ -6,11 +6,11 @@ from plain import models
 class Pageview(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
-    url = models.URLField(max_length=255, db_index=True)
+    url = models.URLField(max_length=1024, db_index=True)
     timestamp = models.DateTimeField(db_index=True, auto_now_add=True)
 
-    title = models.CharField(max_length=255, blank=True)
-    referrer = models.URLField(max_length=255, blank=True)
+    title = models.CharField(max_length=512, blank=True)
+    referrer = models.URLField(max_length=1024, blank=True)
 
     user_id = models.CharField(max_length=255, db_index=True, blank=True)
     session_key = models.CharField(max_length=255, db_index=True, blank=True)
