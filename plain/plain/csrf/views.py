a1f218fccc8621477a3e3deca19dce6c221c2fb7
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-01-23 13:35:32 -0600

    Fix render csrf fail view on non-get requests

--- plain/plain/csrf/views.py
+++ plain/plain/csrf/views.py
@@ -4,7 +4,28 @@ from plain.views import TemplateView
 class CsrfFailureView(TemplateView):
     template_name = "403.html"
 
-    def get(self):
-        response = super().get()
+    def get_response(self):
+        response = super().get_response()
         response.status_code = 403
         return response
+
+    def post(self):
+        return self.get()
+
+    def put(self):
+        return self.get()
+
+    def patch(self):
+        return self.get()
+
+    def delete(self):
+        return self.get()
+
+    def head(self):
+        return self.get()
+
+    def options(self):
+        return self.get()
+
+    def trace(self):
+        return self.get()
