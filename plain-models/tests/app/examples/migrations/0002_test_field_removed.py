515220680f67cca7d513515a6be20d98f59a1894
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-02-25 11:08:01 -0600

    Add a test case for model field being removed but used in RunPython

---
+++ plain-models/tests/app/examples/migrations/0002_test_field_removed.py
@@ -0,0 +1,30 @@
+import uuid
+
+from plain import models
+from plain.models import migrations
+
+
+def set_uuids(models, schema_editor):
+    Car = models.get_model("examples", "Car")
+    for sq in Car.objects.filter(uuid__isnull=True):
+        sq.uuid = uuid.uuid4()
+        sq.save(clean_and_validate=False)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("examples", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name="car",
+            name="uuid",
+            field=models.UUIDField(null=True, editable=False),
+        ),
+        migrations.RunPython(set_uuids),
+        migrations.RemoveField(
+            model_name="car",
+            name="uuid",
+        ),
+    ]
