92305cfb75ec627ed77f88f3c2b6741e38a084eb
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-05-23 10:17:16 -0500

    Use CliRunner with prog name

--- plain/tests/test_cli.py
+++ plain/tests/test_cli.py
@@ -1,11 +1,17 @@
-import subprocess
+from click.testing import CliRunner
+
+from plain.cli.core import cli
 
 
 def test_plain_cli_help():
-    output = subprocess.check_output(["plain", "--help"])
-    assert output.startswith(b"Usage: plain")
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--help"], prog_name="plain")
+    assert result.exit_code == 0
+    assert result.output.startswith("Usage: plain")
 
 
 def test_plain_cli_build():
-    output = subprocess.check_output(["plain", "build"])
-    assert b"Compiled 0 assets into 0 files" in output
+    runner = CliRunner()
+    result = runner.invoke(cli, ["build"], prog_name="plain")
+    assert result.exit_code == 0
+    assert "Compiled 0 assets into 0 files" in result.output
