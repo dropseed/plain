91f3541770f92c64c6643c7962c40e48f45a3fae
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-06-06 13:13:47 -0500

    Remove BaseConnectionHandler class

--- plain-models/plain/models/db.py
+++ plain-models/plain/models/db.py
@@ -124,8 +124,13 @@ class ConnectionDoesNotExist(Exception):
     pass
 
 
-class BaseConnectionHandler:
-    settings_name = None
+class ConnectionHandler:
+    """
+    Handler for database connections. Provides lazy connection creation
+    and convenience methods for managing multiple database connections.
+    """
+
+    settings_name = "DATABASES"
     exception_class = ConnectionDoesNotExist
 
     def __init__(self, settings=None):
@@ -137,13 +142,39 @@ class BaseConnectionHandler:
         self._settings = self.configure_settings(self._settings)
         return self._settings
 
-    def configure_settings(self, settings):
-        if settings is None:
-            settings = getattr(plain_settings, self.settings_name)
-        return settings
+    def configure_settings(self, databases):
+        if databases is None:
+            databases = getattr(plain_settings, self.settings_name)
+        if DEFAULT_DB_ALIAS not in databases:
+            raise ImproperlyConfigured(
+                f"You must define a '{DEFAULT_DB_ALIAS}' database."
+            )
+
+        # Configure default settings.
+        for conn in databases.values():
+            conn.setdefault("AUTOCOMMIT", True)
+            conn.setdefault("CONN_MAX_AGE", 0)
+            conn.setdefault("CONN_HEALTH_CHECKS", False)
+            conn.setdefault("OPTIONS", {})
+            conn.setdefault("TIME_ZONE", None)
+            for setting in ["NAME", "USER", "PASSWORD", "HOST", "PORT"]:
+                conn.setdefault(setting, "")
+
+            test_settings = conn.setdefault("TEST", {})
+            default_test_settings = [
+                ("CHARSET", None),
+                ("COLLATION", None),
+                ("MIRROR", None),
+                ("NAME", None),
+            ]
+            for key, value in default_test_settings:
+                test_settings.setdefault(key, value)
+        return databases
 
     def create_connection(self, alias):
-        raise NotImplementedError("Subclasses must implement create_connection().")
+        db = self.settings[alias]
+        backend = import_module(f"{db['ENGINE']}.base")
+        return backend.DatabaseWrapper(db, alias)
 
     def __getitem__(self, alias):
         try:
@@ -175,43 +206,6 @@ class BaseConnectionHandler:
     def close_all(self):
         for conn in self.all(initialized_only=True):
             conn.close()
-
-
-class ConnectionHandler(BaseConnectionHandler):
-    settings_name = "DATABASES"
-
-    def configure_settings(self, databases):
-        databases = super().configure_settings(databases)
-        if DEFAULT_DB_ALIAS not in databases:
-            raise ImproperlyConfigured(
-                f"You must define a '{DEFAULT_DB_ALIAS}' database."
-            )
-
-        # Configure default settings.
-        for conn in databases.values():
-            conn.setdefault("AUTOCOMMIT", True)
-            conn.setdefault("CONN_MAX_AGE", 0)
-            conn.setdefault("CONN_HEALTH_CHECKS", False)
-            conn.setdefault("OPTIONS", {})
-            conn.setdefault("TIME_ZONE", None)
-            for setting in ["NAME", "USER", "PASSWORD", "HOST", "PORT"]:
-                conn.setdefault(setting, "")
-
-            test_settings = conn.setdefault("TEST", {})
-            default_test_settings = [
-                ("CHARSET", None),
-                ("COLLATION", None),
-                ("MIRROR", None),
-                ("NAME", None),
-            ]
-            for key, value in default_test_settings:
-                test_settings.setdefault(key, value)
-        return databases
-
-    def create_connection(self, alias):
-        db = self.settings[alias]
-        backend = import_module(f"{db['ENGINE']}.base")
-        return backend.DatabaseWrapper(db, alias)
 
 
 class ConnectionRouter:
