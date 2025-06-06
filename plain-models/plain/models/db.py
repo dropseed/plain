54656baf6aa4baaadb869cf37b6cc3b5c87de891
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-06-06 13:00:03 -0500

    Remove old databases attr on ConnectionHandler

--- plain-models/plain/models/db.py
+++ plain-models/plain/models/db.py
@@ -160,13 +160,6 @@ class ConnectionHandler(BaseConnectionHandler):
                 test_settings.setdefault(key, value)
         return databases
 
-    @property
-    def databases(self):
-        # Maintained for backward compatibility as some 3rd party packages have
-        # made use of this private API in the past. It is no longer used within
-        # Plain itself.
-        return self.settings
-
     def create_connection(self, alias):
         db = self.settings[alias]
         backend = load_backend(db["ENGINE"])
