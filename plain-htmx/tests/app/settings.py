56b5e006cff7c4668b64b40a349fef77c7bf348f
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-06-06 12:39:10 -0500

    Enable htmx tests

---
+++ plain-htmx/tests/app/settings.py
@@ -0,0 +1,5 @@
+SECRET_KEY = "test"
+URLS_ROUTER = "app.urls.AppRouter"
+INSTALLED_PACKAGES = [
+    "plain.htmx",
+]
--- plain-htmx/tests/test_views.py
+++ plain-htmx/tests/test_views.py
@@ -1,29 +1,30 @@
 from plain.htmx.views import HTMXViewMixin
 from plain.http import Response
+from plain.test import RequestFactory
 from plain.views import View
 
 
 class V(HTMXViewMixin, View):
-    def get(self, request):
+    def get(self):
         return Response("Ok")
 
 
-def test_is_htmx_request(rf):
-    request = rf.get("/", HTTP_HX_REQUEST="true")
+def test_is_htmx_request():
+    request = RequestFactory().get("/", HTTP_HX_REQUEST="true")
     view = V()
     view.setup(request)
     assert view.is_htmx_request()
 
 
-def test_bhx_fragment(rf):
-    request = rf.get("/", HTTP_BHX_FRAGMENT="main")
+def test_plain_hx_fragment():
+    request = RequestFactory().get("/", HTTP_PLAIN_HX_FRAGMENT="main")
     view = V()
     view.setup(request)
     assert view.get_htmx_fragment_name() == "main"
 
 
-def test_bhx_action(rf):
-    request = rf.get("/", HTTP_BHX_ACTION="create")
+def test_plain_hx_action():
+    request = RequestFactory().get("/", HTTP_PLAIN_HX_ACTION="create")
     view = V()
     view.setup(request)
     assert view.get_htmx_action_name() == "create"
