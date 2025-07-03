61bd7357372edd771980057ef86385110a58f613
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-07-03 16:36:42 -0500

    Add some auth view tests

---
+++ plain-auth/tests/app/settings.py
@@ -0,0 +1,18 @@
+SECRET_KEY = "test"
+URLS_ROUTER = "app.urls.AppRouter"
+INSTALLED_PACKAGES = [
+    "plain.auth",
+    "plain.sessions",
+    "plain.models",
+    "app.users",
+]
+DATABASE = {
+    "ENGINE": "plain.models.backends.sqlite3",
+    "NAME": ":memory:",
+}
+MIDDLEWARE = [
+    "plain.sessions.middleware.SessionMiddleware",
+    "plain.auth.middleware.AuthenticationMiddleware",
+]
+AUTH_LOGIN_URL = "login"
+AUTH_USER_MODEL = "users.User"
