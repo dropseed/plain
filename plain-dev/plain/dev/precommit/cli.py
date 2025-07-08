0a5ffc6de5da7738ae878a3cb6f4e7452a3b5d0b
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-07-08 13:39:33 -0500

    Add logs, auto start of dev services, and background mode to plain dev

--- plain-dev/plain/dev/precommit/cli.py
+++ plain-dev/plain/dev/precommit/cli.py
@@ -10,8 +10,6 @@ import click
 from plain.cli import register_cli
 from plain.cli.print import print_event
 
-from ..services import Services
-
 
 def install_git_hook():
     hook_path = os.path.join(".git", "hooks", "pre-commit")
@@ -38,58 +36,57 @@ def cli(install):
 
     pyproject = Path("pyproject.toml")
 
-    with Services():
-        if pyproject.exists():
-            with open(pyproject, "rb") as f:
-                pyproject = tomllib.load(f)
-            for name, data in (
-                pyproject.get("tool", {})
-                .get("plain", {})
-                .get("pre-commit", {})
-                .get("run", {})
-            ).items():
-                cmd = data["cmd"]
-                print_event(f"Custom: {name} -> {cmd}")
-                result = subprocess.run(cmd, shell=True)
-                if result.returncode != 0:
-                    sys.exit(result.returncode)
-
-        # Run this first since it's probably the most likely to fail
-        if find_spec("plain.code"):
-            check_short("Running plain code checks", "plain", "code", "check")
-
-        if Path("uv.lock").exists():
-            check_short("Checking uv.lock", "uv", "lock", "--check")
-
-        if plain_db_connected():
-            check_short(
-                "Running preflight checks",
-                "plain",
-                "preflight",
-                "--database",
-            )
-            check_short("Checking Plain migrations", "plain", "migrate", "--check")
-            check_short(
-                "Checking for Plain models missing migrations",
-                "plain",
-                "makemigrations",
-                "--dry-run",
-                "--check",
-            )
-        else:
-            check_short("Running Plain checks (without database)", "plain", "preflight")
-            click.secho("--> Skipping migration checks", bold=True, fg="yellow")
-
-        print_event("Running plain build")
-        result = subprocess.run(["plain", "build"])
-        if result.returncode != 0:
-            sys.exit(result.returncode)
-
-        if find_spec("plain.pytest"):
-            print_event("Running tests")
-            result = subprocess.run(["plain", "test"])
+    if pyproject.exists():
+        with open(pyproject, "rb") as f:
+            pyproject = tomllib.load(f)
+        for name, data in (
+            pyproject.get("tool", {})
+            .get("plain", {})
+            .get("pre-commit", {})
+            .get("run", {})
+        ).items():
+            cmd = data["cmd"]
+            print_event(f"Custom: {name} -> {cmd}")
+            result = subprocess.run(cmd, shell=True)
             if result.returncode != 0:
                 sys.exit(result.returncode)
+
+    # Run this first since it's probably the most likely to fail
+    if find_spec("plain.code"):
+        check_short("Running plain code checks", "plain", "code", "check")
+
+    if Path("uv.lock").exists():
+        check_short("Checking uv.lock", "uv", "lock", "--check")
+
+    if plain_db_connected():
+        check_short(
+            "Running preflight checks",
+            "plain",
+            "preflight",
+            "--database",
+        )
+        check_short("Checking Plain migrations", "plain", "migrate", "--check")
+        check_short(
+            "Checking for Plain models missing migrations",
+            "plain",
+            "makemigrations",
+            "--dry-run",
+            "--check",
+        )
+    else:
+        check_short("Running Plain checks (without database)", "plain", "preflight")
+        click.secho("--> Skipping migration checks", bold=True, fg="yellow")
+
+    print_event("Running plain build")
+    result = subprocess.run(["plain", "build"])
+    if result.returncode != 0:
+        sys.exit(result.returncode)
+
+    if find_spec("plain.pytest"):
+        print_event("Running tests")
+        result = subprocess.run(["plain", "test"])
+        if result.returncode != 0:
+            sys.exit(result.returncode)
 
 
 def plain_db_connected():
--- plain-dev/plain/dev/services.py
+++ plain-dev/plain/dev/services.py
@@ -1,36 +1,17 @@
 import os
-import subprocess
-import time
 import tomllib
-from importlib.util import find_spec
 from pathlib import Path
-
-import click
 
 from plain.runtime import APP_PATH, PLAIN_TEMP_PATH
 
-from .poncho.manager import Manager as PonchoManager
+from .process import ProcessManager
 from .utils import has_pyproject_toml
 
 
-class ServicesPid:
-    def __init__(self):
-        self.pidfile = PLAIN_TEMP_PATH / "dev" / "services.pid"
+class ServicesProcess(ProcessManager):
+    pidfile = PLAIN_TEMP_PATH / "dev" / "services.pid"
+    log_dir = PLAIN_TEMP_PATH / "dev" / "logs" / "services"
 
-    def write(self):
-        pid = os.getpid()
-        self.pidfile.parent.mkdir(parents=True, exist_ok=True)
-        with self.pidfile.open("w+") as f:
-            f.write(str(pid))
-
-    def rm(self):
-        self.pidfile.unlink()
-
-    def exists(self):
-        return self.pidfile.exists()
-
-
-class Services:
     @staticmethod
     def get_services(root):
         if not has_pyproject_toml(root):
@@ -46,20 +27,10 @@ class Services:
             .get("services", {})
         )
 
-    def __init__(self):
-        self.poncho = PonchoManager()
-
-    @staticmethod
-    def are_running():
-        pid = ServicesPid()
-        return pid.exists()
-
     def run(self):
-        # Each user of Services will have to check if it is running by:
-        # - using the context manager (with Services())
-        # - calling are_running() directly
-        pid = ServicesPid()
-        pid.write()
+        self.write_pidfile()
+        self.prepare_log()
+        self.init_poncho(print)
 
         try:
             services = self.get_services(APP_PATH.parent)
@@ -73,34 +44,5 @@ class Services:
 
             self.poncho.loop()
         finally:
-            pid.rm()
-
-    def __enter__(self):
-        if not self.get_services(APP_PATH.parent):
-            # No-op if no services are defined
-            return
-
-        if self.are_running():
-            click.secho("Services already running", fg="yellow")
-            return
-
-        print("Starting `plain dev services`")
-        self._subprocess = subprocess.Popen(
-            ["plain", "dev", "services"], cwd=APP_PATH.parent
-        )
-
-        if find_spec("plain.models"):
-            time.sleep(0.5)  # Give it a chance to hit on the first try
-            subprocess.check_call(["plain", "models", "db-wait"], env=os.environ)
-        else:
-            # A bit of a hack to wait for the services to start
-            time.sleep(3)
-
-    def __exit__(self, *args):
-        if not hasattr(self, "_subprocess"):
-            return
-
-        self._subprocess.terminate()
-
-        # Flush the buffer so the output doesn't spill over
-        self._subprocess.communicate()
+            self.rm_pidfile()
+            self.close()
