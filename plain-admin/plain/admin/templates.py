43626494a275631ee1ee0d6fa8b4597c7e998fc1
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-10-24 12:38:18 -0500

    Add admin is_package_installed template function

--- plain-admin/plain/admin/templates.py
+++ plain-admin/plain/admin/templates.py
@@ -1,6 +1,7 @@
 from typing import Any
 
-from plain.templates import register_template_filter
+from plain.packages import packages_registry
+from plain.templates import register_template_filter, register_template_global
 
 from .views.registry import registry
 
@@ -8,3 +9,12 @@ from .views.registry import registry
 @register_template_filter
 def get_admin_model_detail_url(obj: Any) -> str | None:
     return registry.get_model_detail_url(obj)
+
+
+@register_template_global
+def is_package_installed(package_name: str) -> bool:
+    try:
+        packages_registry.get_package_config(package_name)
+        return True
+    except LookupError:
+        return False
