860cd30fe677ef1792d3d3ea1440292bfd81166b
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-11-12 22:40:19 -0600

    Remove prepare_default abstractmethod

--- plain-models/plain/models/backends/base/schema.py
+++ plain-models/plain/models/backends/base/schema.py
@@ -375,12 +375,14 @@ class BaseDatabaseSchemaEditor(ABC):
         """
         return False
 
-    @abstractmethod
     def prepare_default(self, value: Any) -> str:
         """
         Only used for backends which have requires_literal_defaults feature
         """
-        ...
+        raise NotImplementedError(
+            "subclasses of BaseDatabaseSchemaEditor for backends which have "
+            "requires_literal_defaults must provide a prepare_default() method"
+        )
 
     def _column_default_sql(self, field: Field) -> str:
         """
