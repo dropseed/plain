960ce394b9557e8084ab91cfcdc44298eb8547ef
Dave Gaeddert <dave.gaeddert@gmail.com>
2025-12-26 12:09:06 -0600

    Admin for api keys

---
+++ plain-api/plain/api/admin.py
@@ -0,0 +1,30 @@
+from plain.admin.views import (
+    AdminModelDetailView,
+    AdminModelListView,
+    AdminViewset,
+    register_viewset,
+)
+
+from .models import APIKey
+
+
+@register_viewset
+class APIKeyViewset(AdminViewset):
+    class ListView(AdminModelListView):
+        nav_section = "API"
+        nav_icon = "key"
+        model = APIKey
+        title = "API keys"
+        fields = [
+            "name",
+            "uuid",
+            "api_version",
+            "created_at__date",
+            "last_used_at__date",
+            "expires_at__date",
+        ]
+        search_fields = ["name", "uuid"]
+
+    class DetailView(AdminModelDetailView):
+        model = APIKey
+        title = "API key"
