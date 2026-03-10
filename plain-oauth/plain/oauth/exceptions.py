26077db4719d0f94dabc5e39b899cb6642538a5e
Dave Gaeddert <dave.gaeddert@gmail.com>
2026-03-09 21:07:49 -0500

    Add type annotations to OAuth exception __init__ methods

--- plain-oauth/plain/oauth/exceptions.py
+++ plain-oauth/plain/oauth/exceptions.py
@@ -12,7 +12,7 @@ class OAuthError(Exception):
     message = "An error occurred during the OAuth process."
     template_name = ""
 
-    def __init__(self, message="", *, provider_key=""):
+    def __init__(self, message: str = "", *, provider_key: str = "") -> None:
         self.provider_key = provider_key
         if message:
             self.message = message
@@ -33,6 +33,8 @@ class OAuthUserAlreadyExistsError(OAuthError):
     message = "A user already exists with this email address. Please log in first and then connect this OAuth provider to the existing account."
     template_name = "oauth/user_already_exists.html"
 
-    def __init__(self, *, provider_key="", user_model_fields=None):
+    def __init__(
+        self, *, provider_key: str = "", user_model_fields: dict | None = None
+    ) -> None:
         self.user_model_fields = user_model_fields or {}
         super().__init__(provider_key=provider_key)
