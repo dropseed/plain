cc3b15a83ce6f720c8c7e1cb6758c53ba9914ef4
Dave Gaeddert <dave.gaeddert@gmail.com>
2026-03-20 12:22:45 -0500

    Add example job and worker dev process

---
+++ example/app/jobs.py
@@ -0,0 +1,12 @@
+import time
+
+from plain.jobs import Job, register_job
+from plain.logs import app_logger
+
+
+@register_job
+class ExampleJob(Job):
+    def run(self):
+        app_logger.info("Example job running", context={"job": "ExampleJob"})
+        time.sleep(1)
+        app_logger.info("Example job finished", context={"job": "ExampleJob"})
